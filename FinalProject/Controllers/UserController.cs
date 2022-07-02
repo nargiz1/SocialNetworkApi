15673c2b2328041365a792ff247ff0b947e27cfb
nargiz1 <nargizer@code.edu.az>
2022-07-02 17:10:14 +0400

    thirteen

--- FinalProject/Controllers/UserController.cs
+++ FinalProject/Controllers/UserController.cs
@@ -220,16 +220,34 @@ namespace FinalProject.Controllers
             user.Education = dto.Education;
             user.Status = dto.Status;
             user.Country = dto.Country;
+            
+            
+            await _userManager.UpdateAsync(user);
+            return Ok(user);
+        }
+        [HttpPost("profilePic")]
+        public async Task<IActionResult> UpdateProfilePic([FromForm] UpdateUserProfilePicDTO dto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var user = await _userManager.FindByEmailAsync(this.User.FindFirstValue(ClaimTypes.Email));
             if (dto.ImageFile != null && Extensions.IsImage(dto.ImageFile) && Extensions.IsvalidSize(dto.ImageFile, 500))
             {
                 user.ImageUrl = await Extensions.Upload(dto.ImageFile, @"images");
             }
+            await _userManager.UpdateAsync(user);
+            return Ok("Profile picture updated!");
+        }
+        [HttpPost("coverPic")]
+        public async Task<IActionResult> UpdateCoverPic([FromForm] UpdateUserCoverPicDTO dto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var user = await _userManager.FindByEmailAsync(this.User.FindFirstValue(ClaimTypes.Email));
             if (dto.CoverPicFile != null && Extensions.IsImage(dto.CoverPicFile) && Extensions.IsvalidSize(dto.CoverPicFile, 500))
             {
                 user.CoverPicUrl = await Extensions.Upload(dto.CoverPicFile, @"images");
             }
             await _userManager.UpdateAsync(user);
-            return Ok(user);
+            return Ok("Cover picture updated!");
         }
         [HttpPost("ForgotPassword")]
         public async Task<IActionResult> ForgotPassword([FromBody] string email)
